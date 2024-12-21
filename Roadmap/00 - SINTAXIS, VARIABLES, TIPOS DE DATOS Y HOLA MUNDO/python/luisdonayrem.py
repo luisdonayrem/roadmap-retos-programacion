59abc9721aa972150719f9e938fc5691e0605c4b
Luis Donayre <117293710+luisdonayrem@users.noreply.github.com>
2024-12-21 00:56:46 -0500

    #00 - Python
    finish

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/luisdonayrem.py
@@ -0,0 +1,25 @@
+
+#https://www.python.org/
+
+"""
+Aca puede escribir un comentario
+dentro de las comillas 
+sin limite
+"""
+# O aca otro comentario por linea 
+
+'''
+aca tambien sirve para hacer comentarios 
+en varias lineas 
+'''
+
+variable = "mi variable" #las variables estan siempre en minusculas
+
+CONSTANTE = "mi constante" #las constantes estan siempre en mayusculas
+
+int = 10
+float = 25.5
+str = "string"
+bool = True
+
+print("Hola Python!")
